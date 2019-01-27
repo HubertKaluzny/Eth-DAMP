0b1dc0a39a3d9ba9e1413fefbc8d09906c63a10e
Hubert Kaluzny <contact@hubertkaluzny.com>
2019-01-27 07:59:37 +0000

    Integrate simple manager demo with Infura

--- example/manager/web/index.js
+++ example/manager/web/index.js
@@ -1,34 +1,44 @@
 const express = require('express');
 const Web3 = require('web3');
 
-const simpleManagerAddress = '';
+const simpleManagerAddress = '0x71cb83f5d3c25d814aca689134013c771bfabea5';
 const simpleManagerInterface = require('./simpleManagerABI.js');
 
-let w3 = new Web3('ws://localhost:8546');
+let web3 = new Web3(
+  // Replace YOUR-PROJECT-ID with a Project ID from your Infura Dashboard
+  new Web3.providers.WebsocketProvider("wss://ropsten.infura.io/ws/v3/c65c07aeb8dd4f9ca18dc94194ac3010")
+);
+
 let app = express();
+
+var users = new Map();
 
 app.set('view engine', 'ejs');
 
-app.listen(3000);
-
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', {users: users});
 });
 
-let contract = new w3.eth.Contract(simpleManagerInterface, simpleManagerAddress);
+let contract = new web3.eth.Contract(simpleManagerInterface, simpleManagerAddress);
 
-contract.events.DepositEvent({}, (error, event) => {
-  
+contract.events.allEvents({}, (error, event) => {
+  if(event.event == "DepositEvent"){
+    let bal = users.get(event.returnValues._account);
+    bal += event.returnValues._value;
+    users.set(event.returnValues._account, bal);
+
+  }else if(event.event == "WithdrawalEvent"){;
+    users.set(event.returnValues._account, event.returnValues._newBalance);
+
+  }else if(event.event == "SubscribeEvent"){
+    users.set(event.returnValues._account, 0);
+
+  }else if(event.event == "UnSubscribeEvent"){
+    users.delete(event.returnValues._account);
+
+  }
 });
 
-contract.events.WithdrawalEvent({}, (error, event) => {
+console.log("application listening on port 3000");
 
-});
-
-contract.events.SubscribeEvent({}, (error, event) => {
-
-});
-
-contract.events.UnSubscribeEvent({}, (error, event) => {
-
-});
+app.listen(3000);
